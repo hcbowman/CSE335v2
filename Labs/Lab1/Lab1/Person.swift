04d5b96623c880eea86415004fec58eac7226838
hunterbowman <hbowman877@hotmail.com>
2021-08-24 21:23:58 -0700

    First commit; includes Lab 1

---
+++ Labs/Lab1/Lab1/Person.swift
@@ -0,0 +1,15 @@
+//
+//  Data.swift
+//  Lab1
+//
+//  Created by Brooke Bowman on 8/24/21.
+//
+
+import Foundation
+
+struct Person {
+    var hieght: Float
+    var weight: Float
+    var bmi: Float
+    
+}
---
+++ Labs/Lab1/Lab1/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  Lab1
+//
+//  Created by Brooke Bowman on 8/24/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var titleLabel: UILabel
+    var heightLabel: UILabel
+    var heightTextField: UITextField
+    var weightLabel: UILabel
+    var wightTextField: UITextField
+    var bmiLabel: UILabel
+    var bmiOutputLabel: UILabel
+    var messageLabel: UILabel
+    var calculateBMIButton: UIButton
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view = UIView()
+        view.backgroundColor = .white
+    }
+
+
+}
+
